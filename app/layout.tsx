97539991c606519f16ef2603769f9324cf80e697
Fermi Davies <105026256+FermiDavies@users.noreply.github.com>
2024-11-06 23:19:38 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "My Photo Gallery",
+  title: "MotionGround Recyclers",
 };
 
 export default function RootLayout({
